8209df54e5c3b264eb3ff9a9f1f7fe2394622ec8
Fabrizio Montoya <fabriziorandall@gmail.com>
2022-03-17 16:50:06 -0500

    Barra de navegación

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BarraNavegacion from "./Componentes/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BarraNavegacion></BarraNavegacion>
+    </div>
+  );
+}
+
+export default App;
