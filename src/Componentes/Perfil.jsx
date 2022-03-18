e2715b46f3ad3ceea592101b850b0d0f9d3630cf
Fabrizio Montoya <fabriziorandall@gmail.com>
2022-03-18 12:16:48 -0500

    Modo EdiciÃon

--- src/Componentes/Perfil.jsx
+++ src/Componentes/Perfil.jsx
@@ -1,28 +1,34 @@
 import { Paper } from "@mui/material";
 import * as React from "react";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
 import foto from "../img/foto.jpeg";
 import Typography from "@mui/material/Typography";
+import ModoEdicion from "./ModoEdición.jsx";
 
 const Perfil = (props) => {
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const handleEdicion = () => {
+    setModoEdicion(true);
+  };
+  const handleGuardar = () => {
+    setModoEdicion(false);
+  };
+
   const [nombres, setNombres] = useState("Anthony Edward");
   const [apellidos, setApellidos] = useState("STARK");
   const [correo, setCorreo] = useState("tony.stark@marve.inc");
   const [ocupacion, setOcupacion] = useState(
-    "Ingeniero Informátcio en Stark Industries Inc."
+    "Ingeniero Informático en Stark Industries Inc."
   );
   const [id, setId] = useState("002576007");
   const [telefono, setTelefono] = useState("999 555 007");
-  const [fechaNacimiento, setFechaNacimiento] = useState("29 / Mayo / 1970");
+  const [fechaNacimiento, setFechaNacimiento] = useState("03/22/1970");
   const [direccion, setDireccion] = useState("10880 Malibu Point");
   const [acercaDeMi, setAcercaDeMi] = useState(
-    `Genio, millonario, filantropo, playboy. 
-    Ingeniero Informático, mecanico y electrónico
-    con intereses en programación y desarrollo web.
-    Cientifico, inventor, amante de la tecnologia.
-    Filántropo apto a la ayuda comunitaria.`
+    `Genio, millonario, filántropo, playboy. Ingeniero Informático, mecánico y electrónico con intereses en programación y desarrollo web. Científico, inventor, amante de la tecnología. Filántropo apto a la ayuda comunitaria.`
   );
 
   return (
@@ -36,66 +42,107 @@ const Perfil = (props) => {
         <h2 style={{ margin: "0" }}> Perfil</h2>{" "}
         <div style={{ display: "inline", justifyContent: "start" }}>
           {" "}
-          <Button variant="outlined" startIcon={<EditIcon />}>
-            EDITAR
-          </Button>
+          {modoEdicion ? (
+            <Button
+              variant="contained"
+              startIcon={<SaveIcon />}
+              onClick={handleGuardar}
+            >
+              GUARDAR
+            </Button>
+          ) : (
+            <Button
+              variant="outlined"
+              startIcon={<EditIcon />}
+              onClick={handleEdicion}
+            >
+              EDITAR
+            </Button>
+          )}
         </div>
       </div>
       <div style={{ marginTop: "10px", display: "flex", flexDirection: "row" }}>
-        <Paper
-          style={{
-            margin: "0 20px",
-            padding: " 10px",
-            borderRadius: "10px",
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-          }}
-        >
-          <Typography variant="h5" gutterBottom component="div">
-            <b> {apellidos}</b>, {nombres}
-          </Typography>
-          <Typography variant="subtitle1" gutterBottom component="div">
-            {correo}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> Ocupación:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {ocupacion}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> N° de Identificación personal:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {id}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> Teléfono/ Celular:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {telefono}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> Fecha de Nacimiento:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {fechaNacimiento}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> Dirección:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {direccion}
-          </Typography>
-          <Typography variant="h6" gutterBottom component="div">
-            <b> Acerca de mi:</b>
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            <p style={{ textAlign: "start", margin: 0 }}> {acercaDeMi}</p>
-          </Typography>
-        </Paper>
+        {modoEdicion ? (
+          <ModoEdicion
+            datos={{
+              nombres: nombres,
+              apellidos: apellidos,
+              correo: correo,
+              telefono: telefono,
+              ocupacion: ocupacion,
+              id: id,
+              direccion: direccion,
+              acercaDeMi: acercaDeMi,
+              fechaNacimiento: fechaNacimiento,
+            }}
+            states={{
+              nombres: setNombres,
+              apellidos: setApellidos,
+              correo: setCorreo,
+              ocupacion: setOcupacion,
+              id: setId,
+              telefono: setTelefono,
+              fechaNacimiento: setFechaNacimiento,
+              direccion: setDireccion,
+              acercaDeMi: setAcercaDeMi,
+            }}
+          />
+        ) : (
+          <Paper
+            style={{
+              margin: "0 20px",
+              padding: " 10px",
+              borderRadius: "10px",
+              width: "50%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Typography variant="h5" gutterBottom component="div">
+              <b> {apellidos}</b>, {nombres}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom component="div">
+              {correo}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> Ocupación:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {ocupacion}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> N° de Identificación personal:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {id}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> Teléfono/ Celular:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {telefono}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> Fecha de Nacimiento:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {fechaNacimiento}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> Dirección:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              {direccion}
+            </Typography>
+            <Typography variant="h6" gutterBottom component="div">
+              <b> Acerca de mi:</b>
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              <p style={{ textAlign: "start", margin: 0 }}> {acercaDeMi}</p>
+            </Typography>
+          </Paper>
+        )}
         <Paper
           style={{
             padding: "40px 0 ",
